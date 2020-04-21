7a44a0f79ce8bbd27f259c174a35e50b142a3679
EC2 Default User <ec2-user@ip-172-31-32-221.us-east-2.compute.internal>
2020-04-21 05:57:58 +0000

    Arreglando

---
+++ deploy.py
@@ -0,0 +1,16 @@
+import boto3
+
+PROJECT = "Upb-CloudFormation"
+
+cf = boto3.client('cloudformation')
+
+with open('template.yaml', 'r') as file:
+    template = file.read()
+
+response = cf.create_stack(
+    StackName=PROJECT,
+    TemplateBody= template
+)
+
+print(response)
+
